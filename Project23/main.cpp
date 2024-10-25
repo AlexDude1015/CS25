dbd482bb0f5f2631b570732987c65ab0529aa749
AlexDude1015 <li.alexander13@gmail.com>
2024-10-24 19:13:22 -0700

    Adding project 25

--- Project23/main.cpp
+++ Project23/main.cpp
@@ -3,17 +3,6 @@
 #include <cctype>
 #include "customer_number.h"
 
-#include "customer_number.h"
-
-/*Scenarios where the input might fail
-
-1) Character count != 6
-
-2) Not two alphabetical characters + four digits.
-
-*/
-
-
 int main()
 {
     std::string user_input = customer_number::get_user_input();
